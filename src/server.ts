8cf1c259098349af0064dec1e419932ab79fbf33
Tim Macdonald <macdonald.tim@gmail.com>
2024-04-05 22:41:48 -0400

    Launched fly.io app

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,28 @@
 import * as express from 'express'
+import * as cors from 'cors';
 import * as fs from 'node:fs'
 
 // set up express web server
 const app = express()
-
-// set up static content
-app.use(express.static('public'))
+app.use(cors());
 
 // last known count
 let count = 0
 
 // Main page
-app.get('/', async(_request, response) => {
-  // increment counter in counter.txt file
-  try {
-    count = parseInt(fs.readFileSync('counter.txt', 'utf-8')) + 1
-  } catch {
-    count = 1
-  }
-
-  fs.writeFileSync('counter.txt', count.toString())
-
-  // render HTML response
-  try {
-    const content = fs.readFileSync('views/index.tmpl', 'utf-8')
-      .replace('@@COUNT@@', count.toString())
-    response.set('Content-Type', 'text/html')
-    response.send(content)
-  } catch (error) {
-    response.send()
-  }
+app.get('/counter', async(req, res) => {
+  res.json({ count });
 })
 
+app.post('/counter', async(req, res) => {
+  count++;
+  res.json({ count });
+})
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
 // Start web server on port 3000
-app.listen(3000, () => {
+app.listen(port, hostname, () => {
   console.log('Server is listening on port 3000')
 })
